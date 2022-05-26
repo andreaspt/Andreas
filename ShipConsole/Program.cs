c025484dee6426b3c1f537269621734b0cf98a39
Andreas Pettas <Andreaspettas@outlook.com>
2022-05-26 13:57:54 +0300

    Add project files.

---
+++ ShipConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to ship load game!");
+
+        }
+    }
+}
---
+++ ShipConsole/Ship.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipConsole
+{
+    class Ship
+    {
+        public int CyclesCount { get; set; }
+        public int CarsCount { get; set; }
+        public int TrucksCount { get; set; }
+        public int TrainsCount { get; set; }
+        public int Capacity { get; set; }
+
+        public Random rnd = new Random();
+
+        
+
+
+        public int getShipLoad()
+        {
+            int fortio;
+            fortio = CyclesCount + CarsCount + TrucksCount + TrainsCount;
+            return fortio;
+        }
+            
+        
+        public Ship()
+        {
+            CyclesCount = 0;
+            CarsCount = 0;
+            TrucksCount = 0;
+            TrainsCount = 0;
+            Capacity = rnd.Next(10);
+
+        }
+
+            
+
+    }
+}
